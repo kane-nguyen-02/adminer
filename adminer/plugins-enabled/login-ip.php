e29308f4a4faf7e3d799586e94b46a86a3119b60
Kane <cuongnd@d-soft.com.vn>
2026-08-21 22:01:05 +0700

    Enhance login-ip plugin to allow empty DB passwords for localhost and RFC1918 networks

--- adminer/plugins-enabled/login-ip.php
+++ adminer/plugins-enabled/login-ip.php
@@ -1,15 +1,37 @@
 <?php
 
 /**
- * Allow empty DB passwords when the browser hits Adminer from localhost.
- * Needed for local Redis/Mongo (and similar) that have no AUTH configured.
+ * Allow empty DB passwords when the browser hits Adminer from localhost
+ * or any RFC1918 private network (machines that can reach each other on LAN).
  *
- * Includes ::ffff:127. because PHP's built-in server often reports IPv4
+ * Includes ::ffff:… because PHP's built-in server often reports IPv4
  * clients as IPv4-mapped IPv6 under network_mode: host.
+ *
+ * 172.16.0.0/12 is listed as 172.16.–172.31. (not "172.") so we do not
+ * accidentally allow non-private 172.x addresses.
  */
 require_once('plugins/login-ip.php');
 
+$private172 = array();
+$private172Mapped = array();
+for ($i = 16; $i <= 31; $i++) {
+	$private172[] = "172.{$i}.";
+	$private172Mapped[] = "::ffff:172.{$i}.";
+}
+
 return new AdminerLoginIp(
-	array('127.', '::1', '::ffff:127.'),
+	array_merge(
+		array(
+			'127.',
+			'::1',
+			'::ffff:127.',
+			'10.',
+			'::ffff:10.',
+			'192.168.',
+			'::ffff:192.168.',
+		),
+		$private172,
+		$private172Mapped
+	),
 	array()
 );
